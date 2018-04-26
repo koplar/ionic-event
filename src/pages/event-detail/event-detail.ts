40a6a78d16b3f2d20c580012f99edda9534a3c62
koplar <koplar@petra.ac.id>
2018-04-26 15:23:13 -0700

    Commit Ionic

--- src/pages/event-detail/event-detail.ts
+++ src/pages/event-detail/event-detail.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**Tambahan */
 import { EventProvider } from '../../providers/event/event';
+import { Alert, AlertController } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
 /**-- */
 
 // untuk menerima paramater yang dikirim oleh halaman lain
@@ -16,9 +18,13 @@ import { EventProvider } from '../../providers/event/event';
 })
 export class EventDetailPage {
   public currentEvent : any = {}; // array [] / {}
+  public guestName : string = '';
+  public guestPicture : string = null;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    public eventProvider : EventProvider) {
+    public eventProvider : EventProvider,
+    public alertCtrl: AlertController,
+    public cameraPlugin : Camera) {
   }
 
   ionViewDidLoad() {
@@ -31,6 +37,92 @@ export class EventDetailPage {
         this.currentEvent = eventSnapshot.val();
         this.currentEvent.id = eventSnapshot.key;
       });
+  }
+
+  //update data event
+  updateEvent(): void {
+    const alert: Alert = this.alertCtrl.create({
+      message: 'Perubahan data Event!',
+      inputs: [
+        {
+          name: 'eventId',
+          placeholder: 'Isikan id event',
+          value: this.currentEvent.id
+        },
+        {
+          name: 'eventName',
+          placeholder: 'Isikan nama event',
+          value: this.currentEvent.name
+        },
+        {
+          name: 'eventPrice',
+          placeholder: 'Isikan harga',
+          value: this.currentEvent.price
+        },
+        {
+          name: 'eventDate',
+          placeholder: 'Isikan tanggal',
+          value: this.currentEvent.date
+        },
+        {
+          name: 'eventContact',
+          placeholder: 'Isikan contact',
+          value: this.currentEvent.contact
+        },
+        {
+          name: 'eventDes',
+          placeholder: 'Isikan keterangan',
+          value: this.currentEvent.des
+        },
+      ],
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: 'Save',
+          handler: data => {
+            this.eventProvider
+              .updateEvent(this.currentEvent.id, data.name, data.date, data.price, data.contact, data.des);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  // mengambil gambar / capture atau gallery
+  takePicture() : void{
+    this.cameraPlugin
+      .getPicture({
+        quality : 95,
+        destinationType:this.cameraPlugin.DestinationType.DATA_URL,
+        sourceType : this.cameraPlugin.PictureSourceType.CAMERA,
+        allowEdit : true,
+        encodingType : this.cameraPlugin.EncodingType.PNG,
+        targetWidth : 500,
+        targetHeight : 500,
+        saveToPhotoAlbum : true
+      })
+      .then(imageData => {
+        this.guestPicture = imageData;
+      },
+      error => {
+        console.log('Error : ' + JSON.stringify(error));
+      }
+    );
+  }
+
+  // menyimpan data peserta
+  addGuest(guestName : String) : void {
+    this.eventProvider
+      .addGuest(
+        this.currentEvent.id,
+        this.guestName,
+        this.guestPicture
+      )
+      .then(newGuest => {
+        this.guestName = '';
+        this.guestPicture = null;
+      })
   }
 
 }
--- src/providers/event/event.ts
+++ src/providers/event/event.ts
@@ -9,6 +9,7 @@ import { Reference, ThenableReference } from '@firebase/database-types';
 @Injectable()
 export class EventProvider {
   public eventListRef : Reference;
+  public eventListRefDetail : Reference;
 
   constructor() {
     console.log('Hello EventProvider Provider');
@@ -47,5 +48,59 @@ export class EventProvider {
     });
   }
 
+  // proses update event
+  updateEvent(
+    eventId: string, eventName : string, eventDate : string,
+    eventPrice : number, eventContact : string,
+    eventDes : string
+  ) : ThenableReference {
+    this.eventListRefDetail = firebase.database().ref(`/eventList/${eventId}`);
+    return this.eventListRefDetail.push({
+      name : eventName,
+      date : eventDate,
+      price : eventPrice * 1, // yang bertipe number harus diikuti * 1
+      contact : eventContact,
+      des : eventDes
+    });
+  }
+
+  /*updateEvent(eventId: string, name: string, date: string, 
+    price : number, contact : string,
+    des : string): Promise<any> {
+      this.eventListRefDetail = firebase.database().ref(`/eventList/${eventId}`);
+      //return this.eventListRefDetail.update({ name,date,price,contact,des });
+      return this.eventListRefDetail.update({ name,date,price,contact,des });  
+  }*/
+
+  // menambah peserta event
+  addGuest(
+    eventId : string,
+    guestName : string,
+    guestPicture : string = null
+  ) : PromiseLike<any> {
+    return this.eventListRef
+      .child(`${eventId}/guestList`)
+      .push({ guestName })
+      .then( newGuest => {
+        this.eventListRef.child(eventId)
+          .transaction(event => {
+            return event;
+          });
+
+          // cek guestPicture kosong atau tidak
+          if(guestPicture != null){ // kalo ada simpan di storage
+            firebase.storage()
+              .ref(`/guestProfile/${newGuest.key}/profilePicture.png`)
+              .putString(guestPicture, 'base64', {
+                contentType:'image/png'
+              })
+              .then(savedPicture => { // simpan data di database firebase di anak dari event yang dipilih
+                this.eventListRef
+                  .child(`${eventId}/guestList/${newGuest.key}/profilePicture`)
+                  .set(savedPicture.downloadURL);
+              })
+          }
+      });
+  }
 
 }
